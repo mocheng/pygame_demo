a1689944b9f601ca20800d604e3ca5a275e70bd7
Morgan Cheng <morgan.cheng@hulu.com>
2019-07-16 09:41:24 +0800

    init with block control

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Game')
+
+x = 0
+y = 0
+width = 40
+height = 40
+velocity = 5
+
+run = True
+while run:
+    pygame.time.delay(50)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and x >= 0:
+        x -= velocity
+
+    if keys[pygame.K_RIGHT] and x <= SCREEN_WIDTH - width:
+        x += velocity
+
+    if keys[pygame.K_UP] and y > 0:
+        y -= velocity
+
+    if keys[pygame.K_DOWN] and y <= SCREEN_HEIGHT - height:
+        y += velocity
+
+    screen.fill((0, 255, 0))
+    pygame.draw.rect(screen, (255, 0, 0), (x, y, width, height))
+    # pygame.display.update()
+    pygame.display.flip()
+
+pygame.quit()
