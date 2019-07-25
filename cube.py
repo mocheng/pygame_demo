e34a7b1c6674dbc9e282c7fa06e8477f1003aef5
Morgan Cheng <morgan.chengmo@gmail.com>
2019-07-25 22:29:21 +0800

    add surface rendering in cube.py

--- cube.py
+++ cube.py
@@ -30,12 +30,48 @@ edges = (
     (5, 7),
 )
 
+surfaces = (
+    (0,1,2,3),
+    (3,2,7,6),
+    (6,7,5,4),
+    (4,5,1,0),
+    (1,5,7,2),
+    (4,0,3,6)
+)
+
+colors = (
+    (1,0,0),
+    (0,1,0),
+    (0,0,1),
+    (0,1,0),
+    (1,1,1),
+    (0,1,1),
+    (1,0,0),
+    (0,1,0),
+    (0,0,1),
+    (1,0,0),
+    (1,1,1),
+    (0,1,1),
+)
+
 def Cube():
+    glBegin(GL_QUADS)
+    for surface in surfaces:
+        x = 0
+        for vertex in surface:
+            x += 1
+            glColor3fv(colors[x])
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
+            #glColor3fv(colors[x])
             glVertex3fv(vertices[vertex])
     glEnd()
+
+
 
 def main():
     pygame.init()
@@ -53,6 +89,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+                return
 
         glRotate(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
